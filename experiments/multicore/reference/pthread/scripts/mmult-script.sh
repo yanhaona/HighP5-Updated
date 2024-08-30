39210179de21a8dff94a9f5c055def74cda771b1
Muhammad Nur Yanhaona <nur.yanhaona@bracu.ac.bd>
2024-08-30 09:48:07 +0600

    Added a Pthread scripts for MMult

---
+++ experiments/multicore/reference/pthread/scripts/mmult-script.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# keep track of the current directory
+curr_dir=`pwd`
+
+# adjust these two variables based on the mode of the experiment
+ref_target=brac-node
+parallelism=( 20 12 8 6 2 1 )
+executable=../executables/pthread-mmult.o
+array_generator=../../../../../tools/array-generator
+
+# parallel run counts and input size configuration
+parallel_runs=2
+input_size=1024
+cache_block=64
+
+# make data directory for experiments
+mkdir -p ../../../$ref_target/data/pthread/MMult
+root_data_dir=../../../$ref_target/data/pthread/MMult
+
+# clean previous content of the data directory if exists
+rm -rf ${root_data_dir}/*
+
+# copy the executable in the root data directory
+cp $executable $root_data_dir/pthread_exec.o
+
+# generate reference input data files using the array generator
+./${array_generator} $input_size*$input_size double 0.25 1000 $root_data_dir/m1.txt
+./${array_generator} $input_size*$input_size double 0.25 1000 $root_data_dir/m2.txt
+
+for version in "${parallelism[@]}"
+do
+	echo "--------------------------------------------------------------------------------------------------------"
+	echo "running pthread $version-way parallel experiments"
+	echo "Executable $executable"
+	
+	# create a subdirectory for the current degree of parallelism
+	mkdir ${root_data_dir}/version-${version}
+	exper_dir=${root_data_dir}/version-${version}
+
+	# go to the experiment directory
+	cd $exper_dir
+
+	# complete the parallel runs
+	for (( run=1; run<=parallel_runs; run++ ))
+	do
+		echo ""
+		echo ""
+		echo "Experiment run $run"
+
+		# create experiment sub-directory
+		mkdir run-${run}
+		# go inside that directory
+		cd run-${run}
+
+		# create an input file to pass user inputs
+		echo ../../m1.txt > input.txt
+		echo ../../m2.txt >> input.txt
+
+		# execute the program using the input file piping command line inputs
+		../../pthread_exec.o m1 m2 $cache_block $version < input.txt > output.txt
+		cat output.txt
+
+		# come back to the parent directory
+		cd ..
+	done	
+	
+	echo ""
+
+	# come back to the current directory
+	cd $curr_dir
+done
---
+++ experiments/multicore/tozammel-hpm/scripts/bluf-random-script.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# keep track of the current directory
+curr_dir=`pwd`
+
+# adjust these two variables based on the mode of the experiment
+exe_class=random
+mode=../executables/random-affinity
+
+# parallel run counts and input size configuration
+parallel_runs=2
+input_size=1024
+cache_block=64
+
+bluf_dir=${mode}/Bluf
+parallelism=( 1 2 64 128 )
+
+# make data directory for experiments
+mkdir -p ../data/${exe_class}/Bluf
+root_data_dir=../data/${exe_class}/Bluf
+
+# clean previous content of the data directory if exists
+rm -rf ${root_data_dir}/*
+
+for version in "${parallelism[@]}"
+do
+	# find the location of the executable file
+	executable=${bluf_dir}/bluf-${exe_class}-${version}-way.o
+	echo "--------------------------------------------------------------------------------------------------------"
+	echo "running $version-way parallel experiments"
+	echo "Executable $executable"
+	
+	# create a subdirectory for the current executable
+	mkdir ${root_data_dir}/version-${version}
+	exper_dir=${root_data_dir}/version-${version}
+
+	# copy the executable in the created directory
+	cp $executable $exper_dir/executable.o
+
+	# go to the experiment directory
+	cd $exper_dir
+
+	# complete the parallel runs
+	for (( run=1; run<=parallel_runs; run++ ))
+	do
+		echo ""
+		echo ""
+		echo "Experiment run $run"
+
+		# create experiment sub-directory
+		mkdir run-${run}
+		# go inside that directory
+		cd run-${run}
+
+		# create data file input
+		# random initialization for the input matrix
+		echo N > input.txt
+		let lastIndex=$input_size-1
+		echo 0:$lastIndex*0:$lastIndex >> input.txt
+		# provide the block partition parameter of block-stride partition
+		echo $cache_block >> input.txt
+		# disable storing of output and input matrices
+		echo N >> input.txt
+		echo N >> input.txt
+		echo N >> input.txt
+		# disable storing of the pivot array
+		echo N >> input.txt
+
+		# execute the program using the input file piping command line inputs
+		../executable.o < input.txt > output.txt
+		cat output.txt
+
+		# come back to the parent directory
+		cd ..
+	done	
+	
+	echo ""
+
+	# come back to the current directory
+	cd $curr_dir
+done
